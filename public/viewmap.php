52fb4c9781dc412530d96369d525f10332b738da
Weiming Li <weiming66@gmail.com>
2012-02-08 20:48:58 +0800

    first commit

--- public/viewmap.php
+++ public/viewmap.php
@@ -1,153 +1,253 @@
-<?php 
-/*
-	Copyright (C) 2010  Felix Nyffenegger
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-include_once("inc/config.inc.php");
-
-$wiki = "";
-$topic = "";
-if (isset($_GET["wiki"])) {
-	$wiki = $_GET["wiki"];
+<?php
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 1, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/**
+ *	© Copyright 2011 Hewlett-Packard Development Company L.P. 
+ */
+session_start ();
+error_reporting(0);
+ 
+ if (isset ( $_GET ['topic'] ) && isset ( $_GET ['wiki'] )) {
+		if ($_GET ['topic'] != 'Next_Page' && $_GET ['topic'] != 'Previous_Page') {
+			require_once 'lib/MyEngine.php';
+		
+		$engine_code = new MyEngine ( $_GET ['wiki'], $_GET ['topic'] );
+		 
+ 		if ($engine_code->getEngine () ==3) {
+			
+			 
+			$_SESSION ['search'] = 1;
+			$_SESSION ['nextPageNo'] = 0;
+			$_SESSION ['offset'] = 0;
+            $_SESSION ['topic'] = $_GET ['topic'];
+                         
+		} else {	
+			
+			$_SESSION ['topic'] = $_GET ['topic'];
+			$_SESSION ['nextPageNo'] = 0;
+			$_SESSION ['search'] = 0;
+			$_SESSION ['offset'] = 0;
+			 
+		}
+	
+	} 
+	
+	if($_GET ['topic'] == 'Next_Page' || $_GET ['topic'] == 'Previous_Page') {
+		 
+		if ($_GET ['topic'] == 'Next_Page') {
+			
+		
+			$_SESSION ['nextPageNo'] ++;
+			$_SESSION ['offset'] = $_SESSION ['nextPageNo'] * 20;
+		
+		}
+		
+		if ($_GET ['topic'] == 'Previous_Page') {
+			
+			$_SESSION ['nextPageNo'] --;
+			$_SESSION ['offset'] = $_SESSION ['nextPageNo'] * 20;
+		
+		}
+	
+	}
 }
-if (isset($_GET["topic"])) {
-	$topic = $_GET["topic"];
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>HPWikiMap</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords"
+	content="Wiki, Wikipedia, Mindmap, Mind-Map, browse, knowledge network">
+<meta name="description"
+	content="WikiMindMap is a tool to browse easily and efficiently in Wiki content, inspired by the mindmap technique">
+<script type="text/javascript" src="flashobject.js"></script>
+<script type="text/javascript">
+/**
+ *	© Copyright 2011 Hewlett-Packard Development Company L.P. 
+ */
+function ifSearch(){
+
+	
+	var searchIndicator=<?=$_SESSION ['search']?>;
+	 
+	if(searchIndicator===1) {
+		document.getElementById('searchIndicator').innerHTML="[Note:  HPWikiMap could not identify an article to match supplied Search string.  This Map indicates a list of articles that are a close match to the supplied search string.] <br>Page: <?php echo $_SESSION ['nextPageNo']+1;?>";
+		return true;
+	}
+	 
+	return false;
+}
+</script>
+<style>
+<!--
+type       ="text /css"
+.Stil1 {
+	font-size: 12px
+}
+
+.Stil2 {
+	font-size: 14px
+}
+
+.Stil8 {
+	font-size: 10px
+}
+
+.Stil10 {
+	font-size: 44px
+}
+
+.Stil11 {
+	font-weight: bold;
+	color: #FFCC00;
+}
+
+.Stil6 {
+	font-size: 48px
+}
+
+body {
+	background-color: #FFFFFF;
+}
+-->
+</style>
+</head>
+
+<body style="font-family: Arial, Helvetica, sans-serif"
+	onLoad="ifSearch();">
+<?php
+if (! isset ( $_GET ['topic'] )) {
+	$_GET ["topic"] = "";
 }
 ?>
- <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-    <head>
-        <title>WikiMindMap</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1";  charset="iso-8895-1">
-        <meta name="keywords" content="Wiki, Wikipedia, Mindmap, Mind-Map, browse, knowledge network">
-        <meta name="description" content="WikiMindMap is a tool to browse easily and efficiently in Wiki content, inspired by the mindmap technique">
-        <script type="text/javascript" src="js/flashobject.js">
+<?php
+
+if (! isset ( $_GET ['wiki'] )) {
+	$_GET ["wiki"] = "";
+}
+?>
+
+<p>
+
+ 
+<div align="center"><img src="img/Banner.jpg" width="20%"></div>
+
+<form name="search" action="index.php" method="get">
+<p align="center">Select a Wiki: <select name="wiki">
+	<option
+		<?
+		echo ($_GET ["wiki"] == "hpedia.hp.com" ? "selected" : "");
+		?>>hpedia.hp.com</option>
+	<option
+		<?
+		echo ($_GET ["wiki"] == "hpedia-dev.lnx.usa.hp.com" ? "selected" : "");
+		?>>hpedia-dev.lnx.usa.hp.com</option>
+</select> Enter your Topic: <input name="topic" type="text"
+	value="<?
+	if($_GET ['topic'] != 'Next_Page' && $_GET ['topic'] != 'Previous_Page'){
+		echo $_GET ["topic"];
+	}else{
+		echo $_SESSION['topic'];
+	}
+	
+	?>"> <input type="submit" value="Search">
+
+</form>
+
+<table width="100%" border="0" cellspacing="0">
+	<tr bgcolor="#C9D7F1">
+		<td colspan="3" style="border-top-color: #000099">
+		<div align="center" class="Stil2">
+		
+	<?php
+	if ($wiki != '' && $topic != '')
+		echo '<div align="left"><a href="getfreemind.php?wiki=' . $_GET ['wiki'] . '&topic=' . $_SESSION['topic'] . '">Download the wikimap as Freemind file</a></div>';
+	else
+		echo '<div align="Center">Result</div>'?>
+		
+   </div>
+		</td>
+	</tr>
+	<tr>
+		<td width="5%">&nbsp;</td>
+		<td width="90%" height="400">
+		<div id="searchIndicator" style="text-align: center; color: red;  margin-bottom: 15px; margin-top: 10px;"></div>
+		<div id="flashcontent"></div> 
+
+		<script type="text/javascript">
+                // <![CDATA[
+				setTimeout("runFlash()", 1);
+				//runFlash();
+				function getWindowHeight() {
+					var windowHeight = 0;
+					if (typeof(window.innerHeight) == 'number') { windowHeight = window.innerHeight; }
+					else {
+						if (document.documentElement && document.documentElement.clientHeight)
+						{ windowHeight = document.documentElement.clientHeight; }
+						else {
+							if (document.body && document.body.clientHeight) { windowHeight = document.body.clientHeight; }
+						}
+					}
+					return windowHeight;
+				}
+				
+				function runFlash() {
+					var h = getWindowHeight();
+					h = h-200;
+
+					document.getElementById("flashcontent").style.height  = h;
+					
+					var fo = new FlashObject("visorFreemind.swf", "visorFreeMind", "100%", h, 6, "#9999ff");
+					fo.addParam("quality", "high");
+					fo.addParam("bgcolor", "#ffffff");
+					fo.addVariable("openUrl", "_blank");
+					fo.addVariable("initLoadFile", "getpages.php?wiki=<?php
+					echo $_GET['wiki'];
+					?>&topic=<?php
+					echo urlencode ( $_SESSION['topic'] );
+					?>");
+					fo.addVariable("startCollapsedToLevel","1");
+					fo.addVariable("mainNodeShape","bubble");
+					fo.write("flashcontent");
+				};
+                // ]]>
+				
         </script>
-        <link rel='stylesheet' type='text/css' href='assets/wmm.css' />
-    </head>
-    <body>
-        <p align="center">
-            <img src="assets/logo.gif">
-        </p>
-        <form name="search" action="viewmap.php" method="get">
-            <p align="center">
-                Select a Wiki:
-                <select name="wiki">
-                    <option <?php echo ($wiki == "ca.wikipedia.org" ? "selected" : ""); ?> >ca.wikipedia.org</option>
-                    <option <?php echo ($wiki == "de.wikipedia.org" ? "selected" : ""); ?> >de.wikipedia.org</option>
-                    <option <?php echo ($wiki == "en.wikipedia.org" ? "selected" : ""); ?> >en.wikipedia.org</option>
-                    <option <?php echo ($wiki == "es.wikipedia.org" ? "selected" : ""); ?> >es.wikipedia.org</option>
-                    <option <?php echo ($wiki == "fr.wikipedia.org" ? "selected" : ""); ?> >fr.wikipedia.org</option>
-                    <option <?php echo ($wiki == "id.wikipedia.org" ? "selected" : ""); ?> >id.wikipedia.org</option>
-                    <option <?php echo ($wiki == "it.wikipedia.org" ? "selected" : ""); ?> >it.wikipedia.org</option>
-                    <option <?php echo ($wiki == "nl.wikipedia.org" ? "selected" : ""); ?> >nl.wikipedia.org</option>
-                    <option <?php echo ($wiki == "pl.wikipedia.org" ? "selected" : ""); ?> >pl.wikipedia.org</option>
-                    <option <?php echo ($wiki == "pt.wikipedia.org" ? "selected" : ""); ?> >pt.wikipedia.org</option>
-                    <option <?php echo ($wiki == "sv.wikipedia.org" ? "selected" : ""); ?> >sv.wikipedia.org</option>
-                </select>
-                Enter your Topic:<input name="topic" type="text" value="<?php echo $topic; ?>">&nbsp;<input type="submit" value="Search">
-            </p>
-        </form>
-        <table width="100%" border="0" cellspacing="0">
-            <tr>
-                <td colspan="3" class="bar">
-                    <div align="left" class="menuText">
-                        &nbsp;<a href="getfreemind.php?Wiki=<?php echo $wiki; ?>&Topic=<?php echo  urlencode($topic); ?>">Download this mindmap as Freemind file</a>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td width="5%">
-                    &nbsp;
-                </td>
-                <td width="90%" height="400">
-                    <div id="flashcontent">
-                        <p>
-                            Flash plugin or Javascript are turned off. 
-                            Activate both  and reload to view the mindmap 
-                        </p>
-                    </div>
-                    <script type="text/javascript">
-                        setTimeout("runFlash()", 1);
-                        //runFlash();
-                        function getWindowHeight(){
-                            var windowHeight = 0;
-                            if (typeof(window.innerHeight) == 'number') {
-                                windowHeight = window.innerHeight;
-                            }
-                            else {
-                                if (document.documentElement && document.documentElement.clientHeight) {
-                                    windowHeight = document.documentElement.clientHeight;
-                                }
-                                else {
-                                    if (document.body && document.body.clientHeight) {
-                                        windowHeight = document.body.clientHeight;
-                                    }
-                                }
-                            }
-                            return windowHeight;
-                        }
-                        
-                        function runFlash(){
-                            var h = getWindowHeight();
-                            h = h - 200;
-                            
-                            document.getElementById("flashcontent").style.height = h;
-                            var fo = new FlashObject("visorFreemind.swf", "visorFreeMind", "100%", h, 6, "#9999ff");
-                            fo.addParam("quality", "high");
-                            fo.addParam("bgcolor", "#ffffff");
-                            fo.addVariable("openUrl", "_blank");
-                            fo.addVariable("initLoadFile", "getpages.php?Wiki=<?php echo $wiki; ?>&Topic=<?php echo  urlencode($topic); ?>");
-                            fo.addVariable("startCollapsedToLevel", "1");
-                            fo.addVariable("mainNodeShape", "bubble");
-                            fo.write("flashcontent");
-                        };
-                    </script>
-                </td>
-                <td width="5%">
-                    &nbsp;
-                </td>
-            </tr>
-            <tr class="bar">
-                <td colspan="3">
-                    <span class="menuText">
-                        <?php 
-							if ($production) {
-								echo '<a href="help.htm">Help</a> <a href="about.htm">About / Support WikiMindMap</a> <a href="contact.htm">Contact</a>';
-							} else
-							{
-								echo 'Development Version';
-							}
-						?>
-                    </span>
-                </td>
-            </tr>
-        </table>
-        <div align="center" class="footerText">
-            All content of the mindmap is derived from the wiki selected above and is licensed under the terms of <a href="http://www.gnu.org/copyleft/fdl.html" target="_blank">GNU Free Documentaion Licence</a>. You can retrieve the original material by a left click on the topic.
-            <br>
-            Copyright (c) 2007 by Felix Nyffenegger 
-        </div>
-        <p>
-            &nbsp;
-        </p>
-        <script type="text/javascript">
-            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-        </script>
-    </body>
+        </td>
+		<td width="5%">&nbsp;</td>
+	</tr>
+	<tr bgcolor="#C9D7F1">
+		<td colspan="3"><span class="Stil2"><a
+			href="mailto:hpwikimapadmin@hp.com">Contact us</a></span></td>
+	</tr>
+
+
+
+</table>
+<div align="center" class="Stil1 Stil8">All content of the mindmap is
+derived from the wiki selected above and is licensed under the terms of
+<a href="http://www.gnu.org/copyleft/fdl.html" target="_blank">GNU Free
+Documentaion Licence</a>.<br>
+Sponsored by the <a href="http://opensource.professions.hp.com">Open
+Source and Linux Profession<img src="img/oslptux.small.jpg"
+	align="middle" border="0"></a><br>
+</div>
+<p>&nbsp;</p>
+</body>
 </html>
+
